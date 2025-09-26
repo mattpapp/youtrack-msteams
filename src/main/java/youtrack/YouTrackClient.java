45998b540a8d354412d7b25bd1ea129829b09cef
Matthew Pappas <matteopappas@gmail.com>
2025-09-27 00:14:54 +0200

    Add HTTP call to YouTrack API

--- src/main/java/youtrack/YouTrackClient.java
+++ src/main/java/youtrack/YouTrackClient.java
@@ -1,10 +1,40 @@
 package youtrack;
 
-public class YouTrackClient {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-  public YouTrackClient(String baseUrl, String token) {}
+public class YouTrackClient {
+  private final String baseUrl;
+  private final String token;
+
+  public YouTrackClient(String baseUrl, String token) {
+    this.baseUrl = baseUrl;
+    this.token = token;
+  }
 
   public int getNotificationCount() {
-    return 0;
+    try {
+      String url = baseUrl + "/api/users/notifications";
+
+      HttpRequest request =
+          HttpRequest.newBuilder()
+              .uri(URI.create(url))
+              .header("Authorization", "Bearer " + token)
+              .GET()
+              .build();
+
+      HttpResponse<String> response =
+          HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+      if (response.statusCode() == 200) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } catch (Exception e) {
+      return -1;
+    }
   }
 }
